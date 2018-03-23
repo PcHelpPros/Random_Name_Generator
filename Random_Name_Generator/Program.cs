9bbe0ea2342671d7e641266f6aeeeb105e1113a2
Justin Morris <PcHelpPros@Gmail.Com>
2018-03-23 09:16:42 -0400

    First Commit

---
+++ Random_Name_Generator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Name_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ClassRoster = {"Joanne", "Elijah", "Justin", "Rick", "Tony", "Mike", "Rudy", "Aaron", "Kevin "}; // Array OF Names To Cycle Through.
+            Random c = new Random();
+
+            int index = c.Next(0, ClassRoster.Length);
+            string randomItem = ClassRoster[index];
+
+            Console.WriteLine(randomItem);
+        }
+    }
+}
